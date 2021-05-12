6e3266ee1e8053f6080662654c3153ee259f973e
cqghb <1689841661@qq.com>
2021-05-13 00:34:02 +0800

    添加码值查询接口

---
+++ src/main/java/com/test/api/api/constant/ErrorMsgConstant.java
@@ -0,0 +1,14 @@
+package com.test.api.api.constant;
+
+/**
+ * @projectName api
+ * @package com.test.api.api.constant
+ * @className ErrorMsgConstant
+ * @description TODO
+ * @auther wangsheng
+ * @creatTime 2021/5/10 11:44 下午
+ * @company 四川省万源市一生活智能科技有限公司
+ * @department 小程序-微信小程序
+ */
+public class ErrorMsgConstant {
+}
