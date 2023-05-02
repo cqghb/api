b1bedcee7add50a61d17c848d4bf35ee938cba55
王胜 <1689841661@qq.com>
2023-05-02 14:35:17 +0800

    添加商品图片上传实现逻辑，并添加版本记录文件。

--- src/main/java/com/test/api/api/file/SkuFileService.java
+++ src/main/java/com/test/api/api/file/SkuFileService.java
@@ -14,44 +14,47 @@ import java.io.File;
 /**
  * @projectName api
  * @package com.test.api.api.file
- * @className HeadPortraitService
- * @description 头像处理服务
+ * @className SkuFileService
+ * @description 商品相关文件上传的服务
  * @auther wangsheng
- * @creatTime 2021/5/20 12:10 上午
- * @company
- * @department
+ * @creatTime 2023/4/29 上午1:58
+ * @company 四川省万源市一生活智能科技有限公司
+ * @department 小程序-微信小程序
  */
-@Component("1")/* 这里的1是前端上传文件组件送的参数 1表示头像*/
-@FileType(CommConstant.FILE_TYPE_HEAD_PORTRAIT)
-public class HeadPortraitService implements FileDetail {
-    protected static Logger logger = LoggerFactory.getLogger(HeadPortraitService.class);
-    // 头像上传路径
-    @Value("${head.portrai.path}")
-    private String headPortraiPath;
+@Component("2")/* 这里的1是前端上传文件组件送的参数 2表示商品展示的图片*/
+@FileType(CommConstant.FILE_TYPE_PRODUCT_DISPLAY_PICTURE)
+public class SkuFileService implements FileDetail{
+    protected static Logger logger = LoggerFactory.getLogger(SkuFileService.class);
 
+    // 保存位置
+    @Value("${sku.picture.path}")
+    private String path;
+    /**
+     * 商品相关文件上传时先上传到临时目录，等审核通过后再移动到正式目录，防止恶意删除文件，
+     * 如果临时目录的文件被恶意删除，影像要小得多。
+     * @param files 上传文件列表
+     * @return
+     */
     @Override
     public String uploadFile(MultipartFile[] files) {
         long fileSize = files[0].getSize();
-        logger.info("头像大小为: " + fileSize);
         String fileType = files[0].getContentType();
-        logger.info("头像类型为: " + fileType);
 
-//        String fileName = files[0].getOriginalFilename();  // 文件名
         // 换个名字存
         String originalFileName = files[0].getOriginalFilename();
         String suffix = originalFileName.substring(originalFileName.lastIndexOf(CommConstant.DOT));
         String fileName = StringUtil.uuid() + suffix;  // 文件名
-        File dest = new File(headPortraiPath + File.separator + fileName);
+        File dest = new File(path + File.separator + fileName);
         if (!dest.getParentFile().exists()) {
             dest.getParentFile().mkdirs();
         }
         try {
             files[0].transferTo(dest);
         } catch (Exception e) {
-            logger.error("文件上传失败:",e);
+            logger.error("文件传失败",e);
             return null;
         }
-        return headPortraiPath + fileName;
+        return path + fileName;
     }
 
     @Override
